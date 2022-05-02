9a89733ea2af0000ccf01cd0dacafee1a19b4ab9
Here East <nastiapiven@gmail.com>
2022-05-02 18:39:26 +0200

    feat: solved Transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(!(Array.isArray(arr))) throw Error(`'arr' parameter must be an instance of the Array!`);
+  if(arr.length === 0) return [];
+  
+  let copy = arr.slice();
+  // let newArr = [];
+
+  let simpleArr = copy.every(el => typeof el === "number");
+  if(simpleArr) return copy;
+
+  if(copy[0] === "--discard-prev" || copy[0] === "--double-prev") {
+    copy.splice(0, 1);
+  }
+
+  if(copy[copy.length - 1] === "--discard-next" || copy[copy.length - 1] === "--double-next") {
+    copy.pop();
+  }
+
+  for(let i = 0; i < copy.length; i++) {
+    if(copy[i] === "--discard-next") {
+      let nextIdx = i + 1;
+      copy.splice(nextIdx, 1);
+      copy.splice(i, 1);
+    }
+
+    if (copy[i] === "--double-prev") {
+      let prev = copy[i - 1];
+      copy.splice(i - 1, 0, prev);
+      copy.splice(i + 1, 1);
+    }
+
+    if (copy[i] === "--double-next") {
+      let next = copy[i + 1];
+      copy.splice(i + 1, 0, next);
+      copy.splice(i, 1);
+    }
+
+    if (copy[i] === "--discard-prev") {
+      let prevIdx = i - 1;
+      copy.splice(prevIdx, 1);
+      copy.splice(i, 1);
+    }
+
+  }
+
+  return copy;
 }
 
 module.exports = {
