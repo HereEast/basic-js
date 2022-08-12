e56a18657bb31e0216241224f85ced2802c3cec0
Here East <nastiapiven@gmail.com>
2022-08-12 22:43:53 +0200

    feat: solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,66 +1,54 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
+
 function transform(arr) {
-  if(!(Array.isArray(arr))) throw Error(`'arr' parameter must be an instance of the Array!`);
-  if(arr.length === 0) return [];
-  
-  let copy = arr.slice();
-  // let newArr = [];
-
-  let simpleArr = copy.every(el => typeof el === "number");
-  if(simpleArr) return copy;
-
-  if(copy[0] === "--discard-prev" || copy[0] === "--double-prev") {
-    copy.splice(0, 1);
-  }
-
-  if(copy[copy.length - 1] === "--discard-next" || copy[copy.length - 1] === "--double-next") {
-    copy.pop();
-  }
-
-  for(let i = 0; i < copy.length; i++) {
-    if(copy[i] === "--discard-next") {
-      let nextIdx = i + 1;
-      copy.splice(nextIdx, 1);
-      copy.splice(i, 1);
+    if (!Array.isArray(arr)) {
+        throw new Error("'arr' parameter must be an instance of the Array!");
     }
 
-    if (copy[i] === "--double-prev") {
-      let prev = copy[i - 1];
-      copy.splice(i - 1, 0, prev);
-      copy.splice(i + 1, 1);
+    if (arr === [] || arr.every(el => typeof el === "number")) {
+        return arr;
     }
+    let result = [];
 
-    if (copy[i] === "--double-next") {
-      let next = copy[i + 1];
-      copy.splice(i + 1, 0, next);
-      copy.splice(i, 1);
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] === "--discard-next") {
+            if (arr[i + 1]) {
+                i++;
+            }
+        } else if (arr[i] === "--discard-prev") {
+            if (arr[i - 1] && arr[i - 2] !== "--discard-next") {
+                result.pop();
+            }
+        } else if (arr[i] === "--double-next") {
+            if (arr[i + 1]) {
+                result.push(arr[i + 1]);
+            }
+        } else if (arr[i] === "--double-prev") {
+            if (arr[i - 1] && arr[i - 2] != "--discard-next") {
+                result.push(arr[i - 1]);
+            }
+        } else {
+            result.push(arr[i]);
+        }
     }
-
-    if (copy[i] === "--discard-prev") {
-      let prevIdx = i - 1;
-      copy.splice(prevIdx, 1);
-      copy.splice(i, 1);
-    }
-
-  }
-
-  return copy;
+    return result;
 }
 
 module.exports = {
-  transform
+    transform
 };
