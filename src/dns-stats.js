4ff3767d0195ef09c5d6e1a86a7a53abf2ba7e1e
Here East <nastiapiven@gmail.com>
2022-09-01 22:34:34 +0200

    feat: solve dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+ function getDNSStats(domains) {
+  if(!domains.length) return {};
+  let dns = {};
+
+  let domArr = domains.map(domain => domain.split(".").reverse());
+  
+  domArr.forEach(domain => {
+    domain[0] = "." + domain[0];
+
+    for(let i = 0; i < domain.length; i++) {
+      let part = domain.slice(0, i + 1).join(".");
+      if(!dns[part]) dns[part] = 1;
+      else dns[part] += 1;
+    }
+  })
+
+  return dns;
 }
 
 module.exports = {
